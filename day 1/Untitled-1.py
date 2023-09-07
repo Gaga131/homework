0aafd1b7d650ff1889bf4c8548a0ff253bcb171c
Gaga131 <144372458+Gaga131@users.noreply.github.com>
2023-09-07 23:52:03 +0400

    Add files via upload

---
+++ day 1/Untitled-1.py
@@ -0,0 +1,14 @@
+name = "gaga"
+
+
+username = "gvelesiani"
+
+print(name + " " + username)
+
+age = 24
+
+print(age)
+
+print(name + " " + username + " " + str(age) + "." + "I love playing football with friends" + " " + "have five dog" + ":" + "snow,toby,scarrlet,bibi,bubu")
+
+
